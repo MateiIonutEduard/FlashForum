3a9e66012a6e77fd269f8876f0add455e9692d21
MateiIonutEduard <ionuteduardm@gmail.com>
2020-11-25 12:47:50 +0200

    Add project files.

---
+++ FlashForum/Models/PostUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Web;
+
+namespace FlashForum.Models
+{
+    public class PostUtils
+    {
+        public static string LoadPost(string data)
+        {
+            string str = @"(http[s]|ftp[s])?:(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+";
+            var all = Regex.Matches(data, str);
+            string result = data;
+
+            foreach(var item in all)
+                result = Regex.Replace(result, str, $"<a href='{item}'>{item}</a>");
+            
+            return result;
+        }
+    }
+}
