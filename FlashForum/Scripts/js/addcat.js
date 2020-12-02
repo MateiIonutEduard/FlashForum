21f3d93e8d23d4672f36070d00c78c64075008a4
MateiIonutEduard <ionuteduardm@gmail.com>
2020-12-02 19:20:25 +0200

    Add project files.

---
+++ FlashForum/Scripts/js/addcat.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function (e) {
+    $('[data-toggle="tooltip"]').tooltip();
+    $("#data-filter").css("visibility", "hidden");
+
+    $("form").submit(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: '/Home/AddCategory',
+            type: 'post',
+            cache: false,
+            data: {
+                'name': $("#name").val(),
+                'content': $("#content").val()
+            },
+            success: function (data) {
+                var success = data.success;
+
+                if (!success)
+                    $(".text-danger").css("visibility", "visible");
+                else {
+                    location.href = '/Home/Index';
+                    $("#data-filter").css("visibility", "visible");
+                }
+            },
+            async: true
+        });
+    });
+});
---
+++ FlashForum/Scripts/js/create.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function () {
+    $("#filter").css("visibility", "hidden");
+
+    var query = location.href;
+    var url = new URL(query);
+
+    var id = url.searchParams.get("id");
+    if (id === null) location.href = '/Home/Index';
+});
+
+$(document).on("submit", "form", function (e) {
+    e.preventDefault();
+    var query = location.href;
+    var url = new URL(query);
+
+    var id = url.searchParams.get("id");
+    if (id === null) location.href = '/Home/Index';
+    $('[name=choose]').val(parseInt(id));
+
+    var table = new FormData();
+    table.append('id', parseInt(id));
+    table.append('subject', $('#name').val());
+    table.append('question', $('#body').val());
+    table.append('view', $("#view").get(0).files[0]);
+
+    $.ajax({
+        url: '/Home/AddTopic',
+        type: 'post',
+        data: table,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            if (data.success) history.back();
+            else $('.text-danger').css('visibility', 'visible');
+        },
+        async: true
+    });
+
+    setTimeout(function () {
+        location.href = `/Home/AllTopics/?id=${parseInt(id)}`;
+    }, 1000);
+});
+
+function OpenFile() {
+    $("#view").click();
+}
